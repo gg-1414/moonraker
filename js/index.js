102ef37f7b7e854a1df6a5b1ad64e9c9ffae0699
Gina Lee <gina@thesistesting.com>
2021-09-03 13:09:28 -0700

    moving fireworks.push(new Firework()) to draw makes a shit ton continously fire

--- js/index.js
+++ js/index.js
@@ -4,7 +4,6 @@ var gravity;
 function setup() {
   createCanvas(400, 300);
   gravity = createVector(0,0.2); // gravity points down, so it should be positive.
-  fireworks.push(new Firework());
   stroke(255);
   strokeWeight(4);
   firework = new Particle(random(width), height);
@@ -12,6 +11,7 @@ function setup() {
 
 function draw() {
   background(51);
+  fireworks.push(new Firework());
   for (var i = 0; i < fireworks.length; i++) {
     fireworks[i].update();
     fireworks[i].show();
