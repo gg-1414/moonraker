1a15721e255a10d9ed377185f875f07c214e8a7c
Gina Lee <gina@thesistesting.com>
2021-09-03 13:23:20 -0700

    make balls disappear once it hits the point of it starting to travel down

--- js/firework.js
+++ js/firework.js
@@ -1,12 +1,22 @@
 function Firework() {
   this.firework = new Particle(random(width), height);
-  
+
   this.update = function() {
-    this.firework.applyForce(gravity);
-    this.firework.update();
+    // actions to take only if firework exists (not null)
+    if (this.firework) {
+      this.firework.applyForce(gravity);
+      this.firework.update();
+          
+      // point in which before it starts moving downwards
+      if (this.firework.vel.y >= 0) {
+        this.firework = null;
+      }
+    }
   }
   
   this.show = function() {
-    this.firework.show();
+    if (this.firework) {
+      this.firework.show();
+    }
   }
 }
