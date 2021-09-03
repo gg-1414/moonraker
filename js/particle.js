d476e63726fad97b0d8548caf22ed6205affa14d
Gina Lee <gina@thesistesting.com>
2021-09-03 14:12:36 -0700

    fades out fireworks

--- js/particle.js
+++ js/particle.js
@@ -1,6 +1,7 @@
 function Particle(x, y, firework) {
   this.pos = createVector(x,y);
   this.firework = firework;
+  this.lifespan = 255;
   
   if (this.firework) {
     this.vel = createVector(0, random(-10,-2));
@@ -17,14 +18,23 @@ function Particle(x, y, firework) {
   
   this.update = function() {
     if (!this.firework) {
-      this.vel.mult(0.85);
+      this.vel.mult(0.9);
+      this.lifespan -= 4; // goes down by 4 each frame
     }
+    
     this.vel.add(this.acc);
     this.pos.add(this.vel);
     this.acc.mult(0);
   }
   
   this.show = function() {
+    if (!this.firework) {
+      strokeWeight(2);
+      stroke(255, this.lifespan);
+    } else {
+      strokeWeight(4);
+      stroke(255); 
+    }
     point(this.pos.x, this.pos.y) // draws particle
   }
 }
