7d36215b0402e79eb001bb25e81885073e94b1fd
Gina Lee <gina@thesistesting.com>
2021-09-03 13:25:16 -0700

    use boolean value to track when it hits the top (firework has exploded)

--- js/firework.js
+++ js/firework.js
@@ -1,21 +1,22 @@
 function Firework() {
   this.firework = new Particle(random(width), height);
-
+  this.exploded = false;
+  
   this.update = function() {
     // actions to take only if firework exists (not null)
-    if (this.firework) {
+    if (!this.exploded) {
       this.firework.applyForce(gravity);
       this.firework.update();
           
       // point in which before it starts moving downwards
       if (this.firework.vel.y >= 0) {
-        this.firework = null;
+        this.exploded = true;
       }
     }
   }
   
   this.show = function() {
-    if (this.firework) {
+    if (!this.exploded) {
       this.firework.show();
     }
   }
